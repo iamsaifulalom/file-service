a2e43469e747b1059678714da9f1612f6c20dff2
Saiful Alom <saifulalom49590@gmail.com>
2025-12-14 15:56:12 +0600

    feat: add authentication

--- src/index.ts
+++ src/index.ts
@@ -2,49 +2,57 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { verifyDomainAndResources } from "./verify-domain-and-resources";
 import { AllowedFileType } from "./domain-policy";
+import { verifyAuth } from "./verify-auth";
 
 export interface BODY {
-	project : string, 
-	fileName : string, 
-	fileType: AllowedFileType
+  project: string,
+  fileName: string,
+  fileType: AllowedFileType
 }
 
 export default {
   async fetch(req: Request, env: any) {
 
-    if (req.method !== "POST") {
-      return new Response("Only POST method allowed", { status: 405 });
+    try {
+      if (req.method !== "POST") {
+        return new Response("Only POST method allowed", { status: 405 });
+      }
+
+      const body: BODY = await req.json();
+
+      verifyAuth(req, env);                // throws Response if unauthorized
+      verifyDomainAndResources(req, body); // throws Response if origin/filetype invalid
+
+      const key = `${body.project}/uploads/${Date.now()}-${body.fileName}`;
+
+      const s3 = new S3Client({
+        region: "auto",
+        endpoint: env.R2_END_POINT,
+        credentials: {
+          accessKeyId: env.R2_ACCESS_KEY_ID,
+          secretAccessKey: env.R2_SECRET_ACCESS_KEY,
+        },
+      });
+
+      const command = new PutObjectCommand({
+        Bucket: "my-first-bucket",
+        Key: key,
+        ContentType: body.fileType,
+      });
+
+      const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 300 });
+
+      return new Response(JSON.stringify({ uploadUrl, key }), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+
+    } catch (error) {
+      if (error instanceof Response) {
+        return error; // send it to client
+      }
+      return new Response("Internal Server Error", { status: 500 });
     }
-
-    const body: BODY = await req.json();
-    const { project, fileName, fileType } = body;
-
-    // verify domain and resource(file type)
-    verifyDomainAndResources(req, body);
-
-    const key = `${project}/uploads/${Date.now()}-${fileName}`;
-
-    // Use env passed to the fetch function
-    const s3 = new S3Client({
-      region: "auto",
-      endpoint: env.R2_END_POINT,
-      credentials: {
-        accessKeyId: env.R2_ACCESS_KEY_ID,
-        secretAccessKey: env.R2_SECRET_ACCESS_KEY,
-      },
-    });
-
-    const command = new PutObjectCommand({
-      Bucket: "my-first-bucket",
-      Key: key,
-      ContentType: fileType,
-    });
-
-    const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 300 });
-
-    return new Response(JSON.stringify({ uploadUrl, key }), {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
-    });
   },
 };
+
